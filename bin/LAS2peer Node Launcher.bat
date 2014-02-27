b9ac5611616c538f94cad2d7f5f35d427a7bb75e
Peter de Lange <peter.de.lange@rwth-aachen.de>
2014-02-27 18:47:12 +0100

    LAS-13 Refactored Start Scripts

--- bin/LAS2peer Node Launcher.bat
+++ bin/LAS2peer Node Launcher.bat
@@ -1,8 +1,11 @@
 cd %~dp0
 cd ..
+mkdir log
 set BASE=%CD%
 set CLASSPATH="%BASE%/lib/*;%BASE%/export/jars/las2peer.jar;%BASE%/service/*"
 
-java -cp %CLASSPATH% %*
+cd %~dp0
 
+java -cp %CLASSPATH% i5.las2peer.tools.L2pNodeLauncher %*
 pause
+
--- bin/start_node.bat
+++
@@ -1,8 +0,0 @@
-cd %~dp0
-cd ..
-mkdir log
-
-cd %~dp0
-start_java.bat i5.las2peer.tools.L2pNodeLauncher %*
-pause
-
