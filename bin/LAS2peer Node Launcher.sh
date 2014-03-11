1b858e37cbd1bdf54c5fa3ebb806f9bba43a5da0
Mohamed El sherif <mohamedalsherif@hotmail.com>
2014-03-11 15:32:24 +0100

    LAS-13 Refactored Unix Start Scrips

--- bin/LAS2peer Node Launcher.sh
+++ bin/LAS2peer Node Launcher.sh
@@ -1,8 +1,21 @@
 #! /bin/sh
 
-BINPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir -p ../log
+BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/../
 
-mkdir -p log
+if [ ! -e "${BASE}export/jars/las2peer.jar" ]; then
+	echo "Do 'ant jars' before running this!"
+	exit
+fi;
 
-${BINPATH}/start_java.sh i5.las2peer.tools.L2pNodeLauncher "$@"
+if [ $(uname -o) = "Cygwin" ]
+then
+    # we're in cygwin
+	export COLOR_DISABLED=1
+	export CLASSPATH="${BASE}lib/*;${BASE}export/jars/las2peer.jar;"
+else
+	# we're somewhere else
+	export CLASSPATH="${BASE}lib/*:${BASE}export/jars/las2peer.jar"
+fi
 
+java -cp "${CLASSPATH}" i5.las2peer.tools.L2pNodeLauncher "$@"
