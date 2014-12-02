0e924c3947ee96f567dd6b47615474100f5650f8
Thomas Cujé <tcuje@freenet.de>
2014-12-02 15:29:44 +0100

    added UserAgent.userData to agent serialization and adapted tests

--- src/main/java/i5/las2peer/security/UserAgent.java
+++ src/main/java/i5/las2peer/security/UserAgent.java
@@ -15,28 +15,23 @@ import i5.simpleXML.Element;
 import i5.simpleXML.Parser;
 import i5.simpleXML.XMLSyntaxException;
 
+import java.io.Serializable;
 import java.security.KeyPair;
 import java.security.PublicKey;
 import java.util.Random;
 
 import org.apache.commons.codec.binary.Base64;
 
-
 /**
  * An UserAgent represent a (End)user of the LAS2peer system.
  * 
- * 
- *
  */
 public class UserAgent extends PassphraseAgent {
 
-
 	private String sLoginName = null;
-	
 	private String sEmail = null;
-	
-	private Object userData = null;
-	
+	private Serializable userData = null;
+
 	/**
 	 * atm constructor for the MockAgent class, just don't know, how agent creation will take place later 
 	 * 
@@ -46,11 +41,12 @@ public class UserAgent extends PassphraseAgent {
 	 * @throws L2pSecurityException
 	 * @throws CryptoException 
 	 */
-	protected UserAgent ( long id, KeyPair pair, String passphrase, byte[] salt ) throws L2pSecurityException, CryptoException{
-		super ( id, pair, passphrase, salt );				
-		
+	protected UserAgent(long id, KeyPair pair, String passphrase, byte[] salt) throws L2pSecurityException,
+			CryptoException {
+		super(id, pair, passphrase, salt);
+
 	}
-	
+
 	/**
 	 * create an agent with a locked private key
 	 * 
@@ -61,19 +57,18 @@ public class UserAgent extends PassphraseAgent {
 	 * @param encodedPrivate
 	 * @param salt
 	 */
-	protected UserAgent ( long id, PublicKey pubKey, byte[] encodedPrivate, byte[] salt ) {
-		super ( id, pubKey, encodedPrivate, salt );
+	protected UserAgent(long id, PublicKey pubKey, byte[] encodedPrivate, byte[] salt) {
+		super(id, pubKey, encodedPrivate, salt);
 	}
-	
-		
+
 	/**
 	 * get the login name stored for this user agent
 	 * @return the user login name
 	 */
-	public String getLoginName () {
+	public String getLoginName() {
 		return sLoginName;
 	}
-	
+
 	/**
 	 * has this user a login name
 	 * @return true, if a login name is assigned
@@ -81,7 +76,7 @@ public class UserAgent extends PassphraseAgent {
 	public boolean hasLogin() {
 		return sLoginName != null;
 	}
-	
+
 	/**
 	 * select a login name for this agent
 	 * @param loginName
@@ -89,70 +84,87 @@ public class UserAgent extends PassphraseAgent {
 	 * @throws L2pSecurityException 
 	 * @throws UserAgentException 
 	 */
-	public void setLoginName ( String loginName ) throws L2pSecurityException, UserAgentException {
-		if ( this.isLocked())
+	public void setLoginName(String loginName) throws L2pSecurityException, UserAgentException {
+		if (this.isLocked())
 			throw new L2pSecurityException("unlock needed first!");
-		
-		if ( loginName != null && loginName.length() < 4 )
-			throw new UserAgentException ( "please use a login name longer than three characters!");
-		
-		if ( loginName != null && ! (loginName.matches("[a-zA-Z].*")) )
-			throw new UserAgentException ( "please use a login name startung with a normal character (a-z or A-Z)");
-		
+
+		if (loginName != null && loginName.length() < 4)
+			throw new UserAgentException("please use a login name longer than three characters!");
+
+		if (loginName != null && !(loginName.matches("[a-zA-Z].*")))
+			throw new UserAgentException("please use a login name startung with a normal character (a-z or A-Z)");
+
 		// TODO!!!!!!!
 		// duplicate check
 		this.sLoginName = loginName;
 	}
-	
-	
+
 	/**
 	 * select an email address to assign to this user agent
 	 * @param email
 	 * @throws L2pSecurityException 
 	 * @throws UserAgentException 
 	 */
-	public void setEmail ( String email ) throws L2pSecurityException, UserAgentException {
-		if ( this.isLocked())
+	public void setEmail(String email) throws L2pSecurityException, UserAgentException {
+		if (this.isLocked())
 			throw new L2pSecurityException("unlock needed first!");
-		
-		if ( email != null && ! email.contains("@"))
-			throw new UserAgentException ( "This email address contains no @ character...");
-		
+
+		if (email != null && !email.contains("@"))
+			throw new UserAgentException("This email address contains no @ character...");
+
 		// TODO!!!!!!!
 		// duplicate check
 		this.sEmail = email.toLowerCase();
 	}
-	
-	
+
+	/**
+	 * Attaches the given object directly to this agent. The
+	 * user data represent a field of this user agent and
+	 * should be used with small values (< 1MB) only.
+	 * Larger byte amounts could handicap the agent handling
+	 * inside the network.
+	 * 
+	 * @param object The user data object to be serialized and attached.
+	 * @throws L2pSecurityException When the user agent is still locked.
+	 */
+	public void setUserData(Serializable object) throws L2pSecurityException {
+		if (this.isLocked())
+			throw new L2pSecurityException("unlock needed first!");
+
+		this.userData = object;
+	}
+
 	@Override
 	public String toXmlString() {
 		try {
 			StringBuffer result = new StringBuffer(
 					"<las2peer:agent type=\"user\">\n"
-					+"\t<id>" + getId() + "</id>\n"
-					+"\t<publickey encoding=\"base64\">"
-					+ SerializeTools.serializeToBase64( getPublicKey()  ) 
-					+"</publickey>\n"
-					+"\t<privatekey encrypted=\""+CryptoTools.getSymmetricAlgorithm() +"\" keygen=\""+CryptoTools.getSymmetricKeygenMethod()+"\">\n"
-					+"\t\t<salt encoding=\"base64\">" + Base64.encodeBase64String(getSalt()) + "</salt>\n"
-					+"\t\t<data encoding=\"base64\">" + getEncodedPrivate() + "</data>\n"
-					+"\t</privatekey>\n"
-			);
-				
-			if ( sLoginName != null)
-				result.append ( "\t<login>" + sLoginName + "</login>\n" );
-			if ( sEmail != null)
-				result.append ( "\t<email>" + sEmail + "</email>\n");
-					
-			result.append( "</las2peer:agent>\n" );
-			
+							+ "\t<id>" + getId() + "</id>\n"
+							+ "\t<publickey encoding=\"base64\">"
+							+ SerializeTools.serializeToBase64(getPublicKey())
+							+ "</publickey>\n"
+							+ "\t<privatekey encrypted=\"" + CryptoTools.getSymmetricAlgorithm() + "\" keygen=\""
+							+ CryptoTools.getSymmetricKeygenMethod() + "\">\n"
+							+ "\t\t<salt encoding=\"base64\">" + Base64.encodeBase64String(getSalt()) + "</salt>\n"
+							+ "\t\t<data encoding=\"base64\">" + getEncodedPrivate() + "</data>\n"
+							+ "\t</privatekey>\n"
+					);
+
+			if (sLoginName != null)
+				result.append("\t<login>" + sLoginName + "</login>\n");
+			if (sEmail != null)
+				result.append("\t<email>" + sEmail + "</email>\n");
+			if (userData != null) {
+				result.append("\t<userdata>" + SerializeTools.serializeToBase64(userData) + "</userdata>\n");
+			}
+
+			result.append("</las2peer:agent>\n");
+
 			return result.toString();
 		} catch (SerializationException e) {
-			throw new RuntimeException ( "Serialization problems with keys");
-		}		
+			throw new RuntimeException("Serialization problems with keys");
+		}
 	}
-
-
 
 	/**
 	 * sets the state of the object from a string representation resulting from
@@ -167,21 +179,21 @@ public class UserAgent extends PassphraseAgent {
 	 * @exception   MalformedXMLException
 	 *
 	 */
-	public static UserAgent createFromXml (String xml) throws MalformedXMLException {
+	public static UserAgent createFromXml(String xml) throws MalformedXMLException {
 		try {
-			Element root = Parser.parse( xml, false);
-			
-			if ( ! "user".equals( root.getAttribute("type")))
+			Element root = Parser.parse(xml, false);
+
+			if (!"user".equals(root.getAttribute("type")))
 				throw new MalformedXMLException("user agent expected");
-			
-			if ( ! "agent".equals( root.getName()))
+
+			if (!"agent".equals(root.getName()))
 				throw new MalformedXMLException("agent expected");
-			return createFromXml ( root );
+			return createFromXml(root);
 		} catch (XMLSyntaxException e) {
 			throw new MalformedXMLException("Error parsing xml string", e);
-		}		
+		}
 	}
-	
+
 	/**
 	 * Create a new UserAgent protected by the given passphrase.
 	 * 
@@ -192,14 +204,14 @@ public class UserAgent extends PassphraseAgent {
 	 * @throws CryptoException
 	 * @throws L2pSecurityException
 	 */
-	public static UserAgent createUserAgent ( String passphrase ) throws CryptoException, L2pSecurityException {
+	public static UserAgent createUserAgent(String passphrase) throws CryptoException, L2pSecurityException {
 		Random r = new Random();
-		
+
 		byte[] salt = CryptoTools.generateSalt();
-		
-		return new UserAgent( r.nextLong(), CryptoTools.generateKeyPair(), passphrase, salt );		
+
+		return new UserAgent(r.nextLong(), CryptoTools.generateKeyPair(), passphrase, salt);
 	}
-	
+
 	/**
 	 * Create a new UserAgent with a given id protected by the given passphrase.
 	 * 
@@ -211,15 +223,13 @@ public class UserAgent extends PassphraseAgent {
 	 * @throws CryptoException
 	 * @throws L2pSecurityException
 	 */
-	public static UserAgent createUserAgent ( long id, String passphrase ) throws CryptoException, L2pSecurityException {
-		
+	public static UserAgent createUserAgent(long id, String passphrase) throws CryptoException, L2pSecurityException {
+
 		byte[] salt = CryptoTools.generateSalt();
-		
-		return new UserAgent( id, CryptoTools.generateKeyPair(), passphrase, salt );		
+
+		return new UserAgent(id, CryptoTools.generateKeyPair(), passphrase, salt);
 	}
-	
-	
-	
+
 	/**
 	 * Sets the state of the object from a string representation resulting from
 	 * a previous {@link #toXmlString} call.
@@ -228,107 +238,112 @@ public class UserAgent extends PassphraseAgent {
 	 *
 	 * @exception   MalformedXMLException
 	 *
-	 */	
-	public static UserAgent createFromXml ( Element root ) throws MalformedXMLException {
-		try {			
+	 */
+	public static UserAgent createFromXml(Element root) throws MalformedXMLException {
+		try {
 			Element elId = root.getFirstChild();
-			long id = Long.parseLong( elId.getFirstChild().getText());
-			
+			long id = Long.parseLong(elId.getFirstChild().getText());
+
 			Element pubKey = root.getChild(1);
-			if ( !pubKey.getName().equals( "publickey" ))
-				throw new MalformedXMLException("public key expected" );
-			if ( ! pubKey.getAttribute("encoding").equals( "base64"))
-				throw new MalformedXMLException("base64 encoding expected" );
-			
-			PublicKey publicKey = (PublicKey) SerializeTools.deserializeBase64 ( pubKey.getFirstChild().getText());
-			
-			Element privKey = root.getChild ( 2 );
-			if ( !privKey.getName().equals( "privatekey" ))
-				throw new MalformedXMLException("private key expected" );
-			if ( ! privKey.getAttribute("encrypted").equals( CryptoTools.getSymmetricAlgorithm() ))
-				throw new MalformedXMLException(CryptoTools.getSymmetricAlgorithm() + " expected" );
-			if ( ! privKey.getAttribute("keygen").equals( CryptoTools.getSymmetricKeygenMethod() ))
-				throw new MalformedXMLException(CryptoTools.getSymmetricKeygenMethod()  + " expected" );
-			
-			Element elSalt= privKey.getFirstChild();
-			if ( !elSalt.getName().equals( "salt" ))
-				throw new MalformedXMLException("salt expected" );
-			if ( ! elSalt.getAttribute("encoding").equals( "base64"))
-				throw new MalformedXMLException("base64 encoding expected" );
-			
-			byte[] salt = Base64.decodeBase64 (elSalt.getFirstChild().getText());
-			
-			
+			if (!pubKey.getName().equals("publickey"))
+				throw new MalformedXMLException("public key expected");
+			if (!pubKey.getAttribute("encoding").equals("base64"))
+				throw new MalformedXMLException("base64 encoding expected");
+
+			PublicKey publicKey = (PublicKey) SerializeTools.deserializeBase64(pubKey.getFirstChild().getText());
+
+			Element privKey = root.getChild(2);
+			if (!privKey.getName().equals("privatekey"))
+				throw new MalformedXMLException("private key expected");
+			if (!privKey.getAttribute("encrypted").equals(CryptoTools.getSymmetricAlgorithm()))
+				throw new MalformedXMLException(CryptoTools.getSymmetricAlgorithm() + " expected");
+			if (!privKey.getAttribute("keygen").equals(CryptoTools.getSymmetricKeygenMethod()))
+				throw new MalformedXMLException(CryptoTools.getSymmetricKeygenMethod() + " expected");
+
+			Element elSalt = privKey.getFirstChild();
+			if (!elSalt.getName().equals("salt"))
+				throw new MalformedXMLException("salt expected");
+			if (!elSalt.getAttribute("encoding").equals("base64"))
+				throw new MalformedXMLException("base64 encoding expected");
+
+			byte[] salt = Base64.decodeBase64(elSalt.getFirstChild().getText());
+
 			Element data = privKey.getChild(1);
-			if ( !data.getName().equals( "data" ))
-				throw new MalformedXMLException("data expected" );
-			if ( ! data.getAttribute("encoding").equals( "base64"))
-				throw new MalformedXMLException("base64 encoding expected" );
-			byte[] encPrivate = Base64.decodeBase64( data.getFirstChild().getText());
-			
-			
-			UserAgent result = new UserAgent ( id, publicKey, encPrivate, salt);
-			
+			if (!data.getName().equals("data"))
+				throw new MalformedXMLException("data expected");
+			if (!data.getAttribute("encoding").equals("base64"))
+				throw new MalformedXMLException("base64 encoding expected");
+			byte[] encPrivate = Base64.decodeBase64(data.getFirstChild().getText());
+
+			UserAgent result = new UserAgent(id, publicKey, encPrivate, salt);
+
 			int cnt = 3;
 			Element login = root.getChild(cnt);
-			if ( login != null && login.getName().equals("login") ) {
+			if (login != null && login.getName().equals("login")) {
 				result.sLoginName = login.getFirstChild().getText();
-				cnt ++;
+				cnt++;
 			}
 
 			Element email = root.getChild(cnt);
-			if ( email != null  ) {
-				if ( ! email.getName().equals("email"))
+			if (email != null) {
+				if (!email.getName().equals("email")) {
 					throw new MalformedXMLException("email or login element expected!");
+				}
 				result.sEmail = email.getFirstChild().getText();
+				cnt++;
 			}
-			
+
+			Element xmlUserData = root.getChild(cnt);
+			if (xmlUserData != null && xmlUserData.getName().equals("userdata")) {
+				String base64UserData = xmlUserData.getFirstChild().getText();
+				result.userData = SerializeTools.deserializeBase64(base64UserData);
+				cnt++;
+			}
+
 			return result;
 		} catch (XMLSyntaxException e) {
 			throw new MalformedXMLException("Error parsing xml string", e);
 		} catch (SerializationException e) {
-			throw new MalformedXMLException("Deserialization problems", e );
-		}		
+			throw new MalformedXMLException("Deserialization problems", e);
+		}
 	}
-	
-	
 
 	@Override
 	public void receiveMessage(Message message, Context context) throws MessageException {
 		try {
 			message.open(this, getRunningAtNode());
 			Object content = message.getContent();
-			
-			if ( content instanceof PingPongContent ) {
-				Message answer = new Message ( message, new PingPongContent() );
-				
+
+			if (content instanceof PingPongContent) {
+				Message answer = new Message(message, new PingPongContent());
+
 				System.out.println("PingPong: sending answer!");
-				
-				getRunningAtNode().sendResponse( answer, message.getSendingNodeId());
+
+				getRunningAtNode().sendResponse(answer, message.getSendingNodeId());
 			} else {
-				System.out.println ( "got message: "  + message.getContent().getClass() + " / " + message.getContent());
-				System.out.println( "response: " + message.getResponseToId());
-				throw new MessageException ( "What to do with this message?!");
+				System.out.println("got message: " + message.getContent().getClass() + " / " + message.getContent());
+				System.out.println("response: " + message.getResponseToId());
+				throw new MessageException("What to do with this message?!");
 			}
 		} catch (L2pSecurityException e) {
-			throw new MessageException ( "Security problems handling the received message", e);
+			throw new MessageException("Security problems handling the received message", e);
 		} catch (EncodingFailedException e) {
-			throw new MessageException ( "encoding problems with sending an answer", e);
+			throw new MessageException("encoding problems with sending an answer", e);
 		} catch (SerializationException e) {
-			throw new MessageException ( "serialization problems with sending an answer", e);
+			throw new MessageException("serialization problems with sending an answer", e);
 		} catch (AgentNotKnownException e) {
 			// just fire and forget
 		} catch (NodeNotFoundException e) {
 			// just fire and forget
 		}
-		
+
 	}
 
 	@Override
 	public void notifyUnregister() {
 		// TODO Auto-generated method stub	
 	}
-	
+
 	/**
 	 * get the email address assigned to this agent
 	 * @return an email address
@@ -336,28 +351,22 @@ public class UserAgent extends PassphraseAgent {
 	public String getEmail() {
 		return sEmail;
 	}
-	
+
 	/**
 	 * has this user a registered email address?
 	 * @return true, if an email address is assigned
 	 */
-	public boolean hasEmail () {
+	public boolean hasEmail() {
 		return sEmail != null;
 	}
-	
-	public Object getUserData(){
+
+	/**
+	 * get the user data assigned to this agent
+	 * 
+	 * @return Returns the user data object
+	 */
+	public Serializable getUserData() {
 		return this.userData;
 	}
-	
-	public void setUserData(Object u){
-		this.userData = u;
-	}
-	
-	
-	
-	
-	
 
-	
-	
 }
--- src/test/java/i5/las2peer/security/UserAgentTest.java
+++ src/test/java/i5/las2peer/security/UserAgentTest.java
@@ -11,163 +11,164 @@ import java.security.NoSuchAlgorithmException;
 
 import org.junit.Test;
 
-
 public class UserAgentTest {
 
 	@Test
-	public void testUnlocking () throws NoSuchAlgorithmException, L2pSecurityException, CryptoException {
+	public void testUnlocking() throws NoSuchAlgorithmException, L2pSecurityException, CryptoException {
 		String passphrase = "A passphrase to unlock";
-		UserAgent a = UserAgent.createUserAgent( passphrase );
-		
+		UserAgent a = UserAgent.createUserAgent(passphrase);
+
 		try {
 			a.returnSecretKey(null); //not possible without unlocking the private key first
-			fail ("SecurityException should have been thrown");
-		} catch ( L2pSecurityException e) {
+			fail("SecurityException should have been thrown");
+		} catch (L2pSecurityException e) {
 			//Should be thrown
 		} catch (SerializationException e) {
-			fail ("SecurityException should have been thrown");
+			fail("SecurityException should have been thrown");
 			e.printStackTrace();
 		}
-		
+
 		try {
-			a.unlockPrivateKey ( "bad passphrase");
-			fail ("SecurityException should have been thrown");
-		} catch ( L2pSecurityException e )  {}
-		
+			a.unlockPrivateKey("bad passphrase");
+			fail("SecurityException should have been thrown");
+		} catch (L2pSecurityException e) {
+		}
+
 		try {
 			a.returnSecretKey(null); //not possible without unlocking the private key first
-			fail ("SecurityException should have been thrown");
-		} catch ( L2pSecurityException e) {
+			fail("SecurityException should have been thrown");
+		} catch (L2pSecurityException e) {
 			//Should be thrown
 		} catch (SerializationException e) {
-			fail ("SecurityException should have been thrown");
+			fail("SecurityException should have been thrown");
 			e.printStackTrace();
 		}
-		a.unlockPrivateKey ( passphrase);
-		
+		a.unlockPrivateKey(passphrase);
+
 		try {
 			a.returnSecretKey(null); //should be possible now
 		} catch (IllegalArgumentException e) {
-			//Well...empty byte array..but ok since no security exception is thrown
-			e.printStackTrace();
+			// Well...empty byte array..but ok since no security exception is thrown
 		} catch (SerializationException e) {
-			fail ("Illegal argument exception should have been thrown");
+			fail("Illegal argument exception should have been thrown");
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
-	public void testPassphraseChange () throws NoSuchAlgorithmException, L2pSecurityException, CryptoException {
+	public void testPassphraseChange() throws NoSuchAlgorithmException, L2pSecurityException, CryptoException {
 		String passphrase = "a passphrase";
 		UserAgent a = UserAgent.createUserAgent(passphrase);
-		
+
 		String sndPass = "ein anderes Passphrase";
 		try {
 			a.changePassphrase(sndPass);
-			fail ( "SecurityException expected");
-		} catch ( L2pSecurityException e) {}
+			fail("SecurityException expected");
+		} catch (L2pSecurityException e) {
+		}
 		a.unlockPrivateKey(passphrase);
-		
+
 		a.changePassphrase(sndPass);
 		a.lockPrivateKey();
-		
+
 		try {
 			a.unlockPrivateKey(passphrase);
-			fail ( "SecurityException expected");
-		} catch ( L2pSecurityException e ){}
-		
+			fail("SecurityException expected");
+		} catch (L2pSecurityException e) {
+		}
+
 		a.unlockPrivateKey(sndPass);
 	}
-	
+
 	@Test
-	public void testXml () throws NoSuchAlgorithmException, L2pSecurityException, MalformedXMLException, CryptoException {
+	public void testXml() throws NoSuchAlgorithmException, L2pSecurityException, MalformedXMLException, CryptoException, UserAgentException {
 		String passphrase = "a pass";
 		UserAgent a = UserAgent.createUserAgent(passphrase);
-		
+		a.unlockPrivateKey(passphrase);
+		a.setEmail("usera@example.org");
+		a.setUserData("This is the user data attachement.");
+
 		String xml = a.toXmlString();
-		System.out.println( xml); 
-		
-		UserAgent b = UserAgent.createFromXml ( xml );
-	
-		b.unlockPrivateKey ( passphrase );
-		
-		assertEquals ( a.getId(), b.getId());
+		System.out.println(xml);
+
+		UserAgent b = UserAgent.createFromXml(xml);
+
+		assertEquals(a.getId(), b.getId());
+		assertEquals(a.getEmail(), b.getEmail());
+		assertEquals(a.getUserData(), b.getUserData());
 	}
-	
-	
-	public void testLogin () throws CryptoException, L2pSecurityException, MalformedXMLException, UserAgentException {
-		
+
+	public void testLogin() throws CryptoException, L2pSecurityException, MalformedXMLException, UserAgentException {
 		UserAgent a = UserAgent.createUserAgent("test");
 		a.unlockPrivateKey("test");
-		
+
 		a.setLoginName("login");
-		
+
 		String xml = a.toXmlString();
-		
+
 		UserAgent andBack = UserAgent.createFromXml(xml);
-		
-		assertEquals ( "login", andBack.getLoginName());
+
+		assertEquals("login", andBack.getLoginName());
 	}
-	
+
 	@Test
-	public void testEmail () throws CryptoException, L2pSecurityException, MalformedXMLException, UserAgentException {
+	public void testEmail() throws CryptoException, L2pSecurityException, MalformedXMLException, UserAgentException {
 		UserAgent a = UserAgent.createUserAgent("test");
 		a.unlockPrivateKey("test");
-		
+
 		a.setEmail("test@bla");
-		
+
 		String xml = a.toXmlString();
-		
+
 		UserAgent andBack = UserAgent.createFromXml(xml);
-		
-		assertEquals ( "test@bla", andBack.getEmail());
+
+		assertEquals("test@bla", andBack.getEmail());
 	}
-	
+
 	@Test
-	public void testEmailAndLogin () throws CryptoException, L2pSecurityException, MalformedXMLException, UserAgentException {
+	public void testEmailAndLogin() throws CryptoException, L2pSecurityException, MalformedXMLException,
+			UserAgentException {
 		UserAgent a = UserAgent.createUserAgent("test");
 		a.unlockPrivateKey("test");
-		
+
 		a.setEmail("test@bla");
 		a.setLoginName("login");
-		
+
 		String xml = a.toXmlString();
-		
+
 		UserAgent andBack = UserAgent.createFromXml(xml);
-		
-		assertEquals ( "test@bla", andBack.getEmail());
-		assertEquals ( "login", andBack.getLoginName());		
+
+		assertEquals("test@bla", andBack.getEmail());
+		assertEquals("login", andBack.getLoginName());
 	}
-	
-	@Test 
-	public void testLoginExceptions () throws L2pSecurityException, CryptoException {
+
+	@Test
+	public void testLoginExceptions() throws L2pSecurityException, CryptoException {
 		UserAgent a = UserAgent.createUserAgent("test");
 		a.unlockPrivateKey("test");
-		
+
 		try {
-			a.setLoginName( "12323");
+			a.setLoginName("12323");
 		} catch (UserAgentException e) {
-			assertTrue ( e.getMessage().contains ( "a-z"));
+			assertTrue(e.getMessage().contains("a-z"));
 		}
 		try {
-			a.setLoginName( "123");
+			a.setLoginName("123");
 		} catch (UserAgentException e) {
-			assertTrue ( e.getMessage().contains ( "longer"));
+			assertTrue(e.getMessage().contains("longer"));
 		}
-		
-		
 	}
-	@Test 
-	public void testEmailExceptions () throws L2pSecurityException, CryptoException {
+
+	@Test
+	public void testEmailExceptions() throws L2pSecurityException, CryptoException {
 		UserAgent a = UserAgent.createUserAgent("test");
 		a.unlockPrivateKey("test");
-		
+
 		try {
-			a.setEmail( "afduaewd");
+			a.setEmail("afduaewd");
 		} catch (UserAgentException e) {
-			assertTrue ( e.getMessage().contains ( "@"));
+			assertTrue(e.getMessage().contains("@"));
 		}
-		
 	}
 
 }
