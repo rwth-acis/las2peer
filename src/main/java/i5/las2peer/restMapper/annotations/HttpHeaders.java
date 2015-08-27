2ae1fa8511b88eb2124b41d84ae16204927d81c0
Thomas Cujé <tcuje@freenet.de>
2015-08-27 14:29:54 +0200

    added annotation description

--- src/main/java/i5/las2peer/restMapper/annotations/HttpHeaders.java
+++ src/main/java/i5/las2peer/restMapper/annotations/HttpHeaders.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Binds all name value pairs of a HTTP header to a resource method parameter. The type T of the annotated parameter must be {@link java.lang.String}.
+ *
+ */
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface HttpHeaders {
