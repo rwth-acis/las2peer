d79ca4ae6cd5c3b556a777dd0881fbb7f45146bd
Thomas Cujé <tcuje@freenet.de>
2015-11-20 00:13:34 +0100

    replace class loader logger with L2pLogger

--- src/main/java/i5/las2peer/classLoaders/Logger.java
+++ src/main/java/i5/las2peer/classLoaders/Logger.java
@@ -1,68 +1,56 @@
 package i5.las2peer.classLoaders;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
+import i5.las2peer.logging.L2pLogger;
 
 public class Logger {
 
-	private static PrintStream logStream = null;
+	private static final L2pLogger logger = L2pLogger.INSTANCE;
 
 	static {
 		String logFile = System.getenv("CL_LOGFILE");
 		if (logFile != null && !logFile.isEmpty()) {
-			try {
-				logStream = new PrintStream(new FileOutputStream(logFile));
-
-				System.out.println("Logging classloading to " + logFile);
-			} catch (FileNotFoundException e) {
-				System.out.println("Error opening cl logfile: " + e);
-			}
+			// TODO use a dedicated log file for classloading
+			logger.warning(
+					"Dedicated class loader log file is specified, but currently not implemented! Please see default log file for log output.");
+//			try {
+//				logStream = new PrintStream(new FileOutputStream(logFile));
+//				System.out.println("Logging classloading to " + logFile);
+//			} catch (FileNotFoundException e) {
+//				System.out.println("Error opening cl logfile: " + e);
+//			}
 		}
 	}
 
 	static void logFinding(ClassLoader who, String classname, Boolean success) {
-		if (logStream != null) {
-			String whoString = getWhoString(who);
-
-			logStream.println("f\t" + success + "\t" + whoString + "\t" + classname);
-		}
-
+		String whoString = getWhoString(who);
+		logger.finer("f\t" + success + "\t" + whoString + "\t" + classname);
 	}
 
 	static void logMessage(ClassLoader who, String classname, String message) {
-		if (logStream != null) {
-			String whoString = getWhoString(who);
-
-			logStream.println("f\t" + "\t" + whoString + "\t" + classname + "\t" + message);
-		}
-
+		String whoString = getWhoString(who);
+		logger.finer("f\t" + "\t" + whoString + "\t" + classname + "\t" + message);
 	}
 
 	static void logLoading(ClassLoader who, String classname, Boolean success, Object add) {
-		if (logStream != null) {
-			String whoString = getWhoString(who);
-
-			logStream.println("l\t" + success + "\t" + whoString + "\t" + classname + "\t" + add);
-		}
+		String whoString = getWhoString(who);
+		logger.finer("l\t" + success + "\t" + whoString + "\t" + classname + "\t" + add);
 	}
 
 	static void logSubLibrary(ClassLoader who, LibraryClassLoader libraryLoader) {
-		if (logStream != null) {
-			String whoString = getWhoString(who);
-			logStream.println("library load\t" + whoString + "\t" + libraryLoader.getLibrary().getIdentifier());
-		}
+		String whoString = getWhoString(who);
+		logger.finer("library load\t" + whoString + "\t" + libraryLoader.getLibrary().getIdentifier());
 	}
 
 	private static String getWhoString(ClassLoader cl) {
-		if (cl instanceof LibraryClassLoader)
+		if (cl instanceof LibraryClassLoader) {
 			return "lcl" + "\t" + ((LibraryClassLoader) cl).getLibrary().getIdentifier();
-		else if (cl instanceof BundleClassLoader)
+		} else if (cl instanceof BundleClassLoader) {
 			return "bcl" + "\t" + ((BundleClassLoader) cl).getMainLibraryIdentifier();
-		else if (cl instanceof L2pClassLoader)
+		} else if (cl instanceof L2pClassLoader) {
 			return "l2p-main" + "\t\t";
-		else
+		} else {
 			return "unkown";
+		}
 	}
 
 }
