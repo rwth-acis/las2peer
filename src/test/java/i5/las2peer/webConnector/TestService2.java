bec9d70897e8e43b1319315a73d27443c072d6d4
Thomas Cujé <tcuje@freenet.de>
2016-01-05 16:38:07 +0100

    replace deprecated api calls

--- src/test/java/i5/las2peer/webConnector/TestService2.java
+++ src/test/java/i5/las2peer/webConnector/TestService2.java
@@ -1,15 +1,16 @@
 package i5.las2peer.webConnector;
 
-import i5.las2peer.api.Service;
-import i5.las2peer.restMapper.RESTMapper;
-import i5.las2peer.restMapper.annotations.ContentParam;
-import i5.las2peer.restMapper.annotations.Version;
-import i5.las2peer.security.UserAgent;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+
+import i5.las2peer.api.Service;
+import i5.las2peer.restMapper.RESTMapper;
+import i5.las2peer.restMapper.annotations.ContentParam;
+import i5.las2peer.restMapper.annotations.Version;
+import i5.las2peer.security.Agent;
+import i5.las2peer.security.UserAgent;
 
 /**
  * Service to test the web connector
@@ -54,8 +55,9 @@ public class TestService2 extends Service
 	@GET
 	@Path("/userinfo")
 	public String getUserInfo() {
-		if (this.getActiveAgent() instanceof UserAgent) {
-			UserAgent u = (UserAgent) this.getActiveAgent();
+		Agent agent = getContext().getMainAgent();
+		if (agent instanceof UserAgent) {
+			UserAgent u = (UserAgent) agent;
 			return "{'id':'" + u.getId() + ", 'name':'" + u.getLoginName() + "','email':'" + u.getEmail() + "'}";
 		} else {
 			return "Shit happened...";
