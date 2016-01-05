86b91476755338743486e62fc9159a9bfed683d3
Thomas Cujé <tcuje@freenet.de>
2016-01-05 13:34:33 +0100

    replace deprecated api calls

--- src/main/java/i5/las2peer/testing/TestService.java
+++ src/main/java/i5/las2peer/testing/TestService.java
@@ -272,7 +272,7 @@ public class TestService extends Service {
 	 */
 	public void storeEnvelopeString(String s)
 			throws UnsupportedEncodingException, EncodingFailedException, DecodingFailedException {
-		cache = new Envelope((String) s, getActiveAgent());
+		cache = new Envelope((String) s, getContext().getMainAgent());
 		cache.close();
 	}
 
