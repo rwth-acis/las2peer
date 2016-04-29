5c6a3ad2b29ea4bf603568a5474dd6768521ad1f
Thomas Cujé <tcuje@freenet.de>
2016-04-29 13:04:29 +0200

    change default message timeout to 30 seconds instead of 1 hour

--- src/main/java/i5/las2peer/tools/L2pNodeLauncher.java
+++ src/main/java/i5/las2peer/tools/L2pNodeLauncher.java
@@ -525,7 +525,7 @@ public class L2pNodeLauncher {
 			throw new IllegalStateException("please log in a valid user with registerUserAgent before!");
 
 		Agent receiver = node.getServiceAgent(ServiceNameVersion.fromString(serviceNameVersion));
-		Message request = new Message(currentUser, receiver, (Serializable) new ListMethodsContent(), 30000);
+		Message request = new Message(currentUser, receiver, (Serializable) new ListMethodsContent());
 		request.setSendingNodeId((NodeHandle) node.getNodeId());
 
 		Message response = node.sendMessageAndWaitForAnswer(request);
