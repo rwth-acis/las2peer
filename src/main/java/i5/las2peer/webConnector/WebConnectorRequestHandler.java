4da3162f85ca37f3876345e60e9ac102b472bb01
Thomas Cujé <tcuje@freenet.de>
2016-05-11 14:00:25 +0200

    fix imports

--- src/main/java/i5/las2peer/webConnector/WebConnectorRequestHandler.java
+++ src/main/java/i5/las2peer/webConnector/WebConnectorRequestHandler.java
@@ -16,7 +16,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 
 import com.nimbusds.oauth2.sdk.ErrorObject;
 import com.nimbusds.oauth2.sdk.ParseException;
