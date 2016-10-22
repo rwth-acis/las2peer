0d0a899c2857a14c1f4ce92297e883a6911b7705
Jasper Nalbach <nalbach@dbis.rwth-aachen.de>
2016-10-22 15:11:08 +0200

    updated javadoc

--- src/main/java/i5/las2peer/restMapper/annotations/ServicePath.java
+++ src/main/java/i5/las2peer/restMapper/annotations/ServicePath.java
@@ -8,9 +8,9 @@ import java.lang.annotation.Target;
 /**
  * The base path where the service is deployed.
  * 
- * This is the base path for the service. All service methods will be available under /servicePath/*.
+ * This is the base path for the service. All service methods will be available under /path/to/my/service/*.
  * 
- * The path must not contain "/".
+ * Please notice that paths of different services have to be prefix free.
  *
  */
 @Target(ElementType.TYPE)
