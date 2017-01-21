b72b42aa2fa1aab43ca050d413d2edf8ee380340
Thomas Cujé <tcuje@freenet.de>
2017-01-21 18:43:58 +0100

    fix NPE on pastry node relaunch

--- src/main/java/i5/las2peer/p2p/PastryNodeImpl.java
+++ src/main/java/i5/las2peer/p2p/PastryNodeImpl.java
@@ -109,7 +109,6 @@ public class PastryNodeImpl extends Node {
 		this.mode = storageMode;
 		this.storageDir = null; // null = SharedStorage chooses directory
 		this.nodeIdSeed = nodeIdSeed;
-		setupPastryEnvironment();
 		this.setStatus(NodeStatus.CONFIGURED);
 	}
 
@@ -150,73 +149,7 @@ public class PastryNodeImpl extends Node {
 		this.mode = storageMode;
 		this.storageDir = storageDir;
 		this.nodeIdSeed = nodeIdSeed;
-		setupPastryEnvironment();
 		this.setStatus(NodeStatus.CONFIGURED);
-	}
-
-	/**
-	 * setup pastry environment settings
-	 */
-	private void setupPastryEnvironment() {
-		pastryEnvironment = new Environment();
-		String[] configFiles = new String[] { "etc/pastry.properties", "config/pastry.properties",
-				"properties/pastry.properties" };
-		String found = null;
-		for (String filename : configFiles) {
-			try {
-				if (new File(filename).exists()) {
-					found = filename;
-					break;
-				}
-			} catch (Exception e) {
-				logger.log(Level.FINER, "Exception while checking for config file '" + filename + "'", e);
-			}
-		}
-		Hashtable<String, String> properties = new Hashtable<>();
-		if (found != null) {
-			System.out.println("Using pastry property file " + found);
-			try {
-				Properties props = new Properties();
-				props.load(new FileInputStream(found));
-
-				for (Object propname : props.keySet()) {
-					properties.put((String) propname, (String) props.get(propname));
-				}
-			} catch (FileNotFoundException e) {
-				System.err.println("Unable to open property file " + found);
-			} catch (IOException e) {
-				System.err.println("Error opening property file " + found + ": " + e.getMessage());
-			}
-		} else {
-			System.out.println("No pastry property file found - using default values");
-		}
-		if (!properties.containsKey("nat_search_policy")) {
-			properties.put("nat_search_policy", "never");
-		}
-		if (!properties.containsKey("firewall_test_policy")) {
-			properties.put("firewall_test_policy", "never");
-		}
-		if (!properties.containsKey("nat_network_prefixes")) {
-			properties.put("nat_network_prefixes", "127.0.0.1;10.;192.168.;");
-		}
-		if (pastryBindAddress != null && pastryBindAddress.isLoopbackAddress()) {
-			properties.put("allow_loopback_address", "1");
-		}
-		if (!properties.containsKey("p2p_past_messageTimeout")) {
-			properties.put("p2p_past_messageTimeout", "120000");
-		}
-		if (!properties.containsKey("pastry_socket_known_network_address")) {
-			if (!properties.containsKey("pastry_socket_known_network_address_port")) {
-				properties.put("pastry_socket_known_network_address_port", "80");
-			}
-		}
-		if (!properties.containsKey("nat_search_policy")) {
-			properties.put("nat_search_policy", "never");
-		}
-		for (String prop : properties.keySet()) {
-			pastryEnvironment.getParameters().setString(prop, properties.get(prop));
-			logger.info("setting: " + prop + ": '" + properties.get(prop) + "'");
-		}
 	}
 
 	/**
@@ -290,8 +223,9 @@ public class PastryNodeImpl extends Node {
 	 */
 	@Override
 	protected void launchSub() throws NodeException {
-
 		try {
+			setupPastryEnvironment();
+
 			setStatus(NodeStatus.STARTING);
 
 			NodeIdFactory nidFactory = null;
@@ -354,6 +288,71 @@ public class PastryNodeImpl extends Node {
 			throw new NodeException("Interrupted while joining pastry ring!", e);
 		} catch (IllegalStateException e) {
 			throw new NodeException("Unable to open Netwock socket - is the port already in use?", e);
+		}
+	}
+
+	/**
+	 * setup pastry environment settings
+	 */
+	private void setupPastryEnvironment() {
+		pastryEnvironment = new Environment();
+		String[] configFiles = new String[] { "etc/pastry.properties", "config/pastry.properties",
+				"properties/pastry.properties" };
+		String found = null;
+		for (String filename : configFiles) {
+			try {
+				if (new File(filename).exists()) {
+					found = filename;
+					break;
+				}
+			} catch (Exception e) {
+				logger.log(Level.FINER, "Exception while checking for config file '" + filename + "'", e);
+			}
+		}
+		Hashtable<String, String> properties = new Hashtable<>();
+		if (found != null) {
+			System.out.println("Using pastry property file " + found);
+			try {
+				Properties props = new Properties();
+				props.load(new FileInputStream(found));
+
+				for (Object propname : props.keySet()) {
+					properties.put((String) propname, (String) props.get(propname));
+				}
+			} catch (FileNotFoundException e) {
+				System.err.println("Unable to open property file " + found);
+			} catch (IOException e) {
+				System.err.println("Error opening property file " + found + ": " + e.getMessage());
+			}
+		} else {
+			System.out.println("No pastry property file found - using default values");
+		}
+		if (!properties.containsKey("nat_search_policy")) {
+			properties.put("nat_search_policy", "never");
+		}
+		if (!properties.containsKey("firewall_test_policy")) {
+			properties.put("firewall_test_policy", "never");
+		}
+		if (!properties.containsKey("nat_network_prefixes")) {
+			properties.put("nat_network_prefixes", "127.0.0.1;10.;192.168.;");
+		}
+		if (pastryBindAddress != null && pastryBindAddress.isLoopbackAddress()) {
+			properties.put("allow_loopback_address", "1");
+		}
+		if (!properties.containsKey("p2p_past_messageTimeout")) {
+			properties.put("p2p_past_messageTimeout", "120000");
+		}
+		if (!properties.containsKey("pastry_socket_known_network_address")) {
+			if (!properties.containsKey("pastry_socket_known_network_address_port")) {
+				properties.put("pastry_socket_known_network_address_port", "80");
+			}
+		}
+		if (!properties.containsKey("nat_search_policy")) {
+			properties.put("nat_search_policy", "never");
+		}
+		for (String prop : properties.keySet()) {
+			pastryEnvironment.getParameters().setString(prop, properties.get(prop));
+			logger.info("setting: " + prop + ": '" + properties.get(prop) + "'");
 		}
 	}
 
