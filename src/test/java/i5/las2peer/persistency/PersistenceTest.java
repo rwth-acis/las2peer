4628443200fe3c341e74f6301c5aad35fb9da168
Thomas Cujé <tcuje@freenet.de>
2017-01-28 12:24:02 +0100

    increase test network size to replication factor fixes versioning test

--- src/test/java/i5/las2peer/persistency/PersistenceTest.java
+++ src/test/java/i5/las2peer/persistency/PersistenceTest.java
@@ -5,9 +5,7 @@ import java.util.ArrayList;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TestName;
 
 import i5.las2peer.p2p.PastryNodeImpl;
 import i5.las2peer.persistency.SharedStorage.STORAGE_MODE;
@@ -19,14 +17,11 @@ public class PersistenceTest {
 
 	private ArrayList<PastryNodeImpl> nodes;
 
-	@Rule
-	public TestName name = new TestName();
-
 	@Before
 	public void startNetwork() {
 		try {
 			// start test node
-			nodes = TestSuite.launchNetwork(3, STORAGE_MODE.FILESYSTEM, true);
+			nodes = TestSuite.launchNetwork(SharedStorage.DEFAULT_NUM_OF_REPLICAS + 1, STORAGE_MODE.FILESYSTEM, true);
 			System.out.println("Test network started");
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -45,11 +40,6 @@ public class PersistenceTest {
 	}
 
 	@Test
-	public void testStartStopNetwork() {
-		// just as time reference ...
-	}
-
-	@Test
 	public void testFilesystemPersistence() {
 		try {
 			// start network and write data into shared storage
@@ -62,7 +52,7 @@ public class PersistenceTest {
 			stopNetwork();
 			Thread.sleep(500);
 			// restart the network and read data
-			nodes = TestSuite.launchNetwork(3, STORAGE_MODE.FILESYSTEM, false);
+			nodes = TestSuite.launchNetwork(SharedStorage.DEFAULT_NUM_OF_REPLICAS + 1, STORAGE_MODE.FILESYSTEM, false);
 			System.out.println("test network restarted");
 			node1 = nodes.get(0);
 			Envelope after = node1.fetchEnvelope("test");
@@ -73,33 +63,47 @@ public class PersistenceTest {
 		}
 	}
 
-	// TODO test currently not working because of LAS-359, time to join the network is too high
-//	@Test
-//	public void testVersioning() {
-//		try {
-//			// start network and write data into shared storage
-//			PastryNodeImpl node1 = nodes.get(0);
-//			Envelope env = node1.createUnencryptedEnvelope("test", "This is las2peer!");
-//			UserAgent smith = MockAgentFactory.getAdam();
-//			smith.unlockPrivateKey("adamspass");
-//			node1.storeEnvelope(env, smith);
-//			// shutdown node 2
-//			PastryNodeImpl node2 = nodes.remove(1);
-//			node2.shutDown();
-//			Thread.sleep(500);
-//			// update envelope
-//			env = node1.createUnencryptedEnvelope(env, "This is las2peer again!");
-//			node1.storeEnvelope(env, smith);
-//			// start node 2 again (still with version 1 of env in storage
-//			node2 = TestSuite.addNode(node1.getPort(), STORAGE_MODE.FILESYSTEM, 1L);
-//			nodes.set(1, node2);
-//			// read data
-//			Envelope fetched = node2.fetchEnvelope("test");
-//			Assert.assertEquals(env.getContent(), fetched.getContent());
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			Assert.fail(e.toString());
-//		}
-//	}
+	@Test
+	public void testVersionSafety() {
+		try {
+			// start network and write data into shared storage
+			PastryNodeImpl node1 = nodes.get(0);
+			Envelope env = node1.createUnencryptedEnvelope("test", "This is las2peer!");
+			UserAgent smith = MockAgentFactory.getAdam();
+			smith.unlockPrivateKey("adamspass");
+			node1.storeEnvelope(env, smith);
+			// shutdown node 2
+			PastryNodeImpl node2 = nodes.remove(1);
+			node2.shutDown();
+			// update envelope
+			Envelope updated = node1.createUnencryptedEnvelope(env, "This is las2peer again!");
+			long start = System.currentTimeMillis();
+			node1.storeEnvelope(updated, smith);
+			long stop = System.currentTimeMillis();
+			System.out.println(stop - start);
+			// start node 2 again (still with version 1 of env in storage
+			node2 = TestSuite.addNode(node1.getPort(), STORAGE_MODE.FILESYSTEM, 1L);
+			nodes.set(1, node2);
+			// read data
+			Envelope fetched = node2.fetchEnvelope("test");
+			Assert.assertEquals(updated.getContent(), fetched.getContent());
+			/*
+			 * The test output should also end with the following lines:
+			 * 
+			 * Looking for metadata envelope with identifier 'test' and version 1 at id FDC232A97E1E3E66C6023E1306DC2C1CA9EFF2F9 ...
+			Lookup got 6 past handles for identifier 'test' and version 1
+			Looking for metadata envelope with identifier 'test' and version 2 at id 1B225008E192E98014E4F24EC6655D160F0AFB86 ...
+			Lookup got 5 past handles for identifier 'test' and version 2
+			Looking for metadata envelope with identifier 'test' and version 3 at id B7B3972C41D6E0E480F4292DA52AD6140B51BFD2 ...
+			Lookup got 0 past handles for identifier 'test' and version 3
+			 * 
+			 * Version 1 should have one more handle than version 2 and version 3 has zero handles.
+			 * 
+			 */
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail(e.toString());
+		}
+	}
 
 }
