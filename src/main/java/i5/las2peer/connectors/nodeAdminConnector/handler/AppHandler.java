1935d51f04759d2f05459932ff55a4a8183a25c3
Thomas Cujé <tcuje@freenet.de>
2017-07-12 12:02:34 +0200

    improve NAC files handling w/ AbstractFilesHandler

--- src/main/java/i5/las2peer/connectors/nodeAdminConnector/handler/AppHandler.java
+++ src/main/java/i5/las2peer/connectors/nodeAdminConnector/handler/AppHandler.java
@@ -1,25 +1,20 @@
 package i5.las2peer.connectors.nodeAdminConnector.handler;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.util.logging.Level;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 
 import i5.las2peer.connectors.nodeAdminConnector.NodeAdminConnector;
-import i5.las2peer.tools.SimpleTools;
 
 @Path(AppHandler.ROOT_ROUTE)
-public class AppHandler extends AbstractHandler {
+public class AppHandler extends AbstractFilesHandler {
 
 	public static final String ROOT_ROUTE = "/webapp";
 	public static final String DEFAULT_ROUTE = ROOT_ROUTE + "/view-status";
@@ -51,37 +46,14 @@ public class AppHandler extends AbstractHandler {
 		}
 	}
 
-	protected Response serveFile(String filename, String myHostname) throws IOException {
-		InputStream is = getClass().getResourceAsStream(filename);
-		if (is == null) {
-			logger.info("File not found: '" + filename + "'");
-			return Response.status(Status.NOT_FOUND).build();
-		}
-		byte[] bytes = SimpleTools.toByteArray(is);
-		String strContent = new String(bytes, StandardCharsets.UTF_8);
+	@Override
+	protected String replaceContent(String filename, String myHostname, String strContent) {
 		if (filename.equalsIgnoreCase(ROOT_ROUTE + "/index.html")) {
 			strContent = strContent.replace("<base href=\"/\">", "<base href=\"" + ROOT_ROUTE + "/\">");
 			// just return host header, so browsers do not block subsequent ajax requests to an possible insecure host
 			strContent = strContent.replace("$connector_address$", myHostname);
 		}
-		String lName = filename.toLowerCase();
-		String mime = "application/octet-stream";
-		if (lName.endsWith(".html") || lName.endsWith(".htm")) {
-			mime = "text/html";
-		} else if (lName.endsWith(".css")) {
-			mime = "text/css";
-		} else if (lName.endsWith(".js")) {
-			mime = "text/javascript";
-		} else if (lName.endsWith(".json")) {
-			mime = "application/json";
-		} else if (lName.endsWith(".ico")) {
-			return Response.ok(bytes).type("image/x-icon").build(); // don't use stringContent, return raw bytes instead
-		} else if (lName.endsWith(".png")) {
-			mime = "image/png";
-		} else {
-			logger.log(Level.WARNING, "Unknown file type '" + filename + "' using " + mime + " as mime");
-		}
-		return Response.ok(strContent).type(mime).build();
+		return strContent;
 	}
 
 }
--- src/main/java/i5/las2peer/connectors/nodeAdminConnector/handler/SwaggerUIHandler.java
+++ src/main/java/i5/las2peer/connectors/nodeAdminConnector/handler/SwaggerUIHandler.java
@@ -1,24 +1,20 @@
 package i5.las2peer.connectors.nodeAdminConnector.handler;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.logging.Level;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 
 import i5.las2peer.connectors.nodeAdminConnector.NodeAdminConnector;
-import i5.las2peer.tools.SimpleTools;
 
 @Path("/swagger-ui")
-public class SwaggerUIHandler extends AbstractHandler {
+public class SwaggerUIHandler extends AbstractFilesHandler {
 
 	public static final String ROOT_PATH = "/swagger-ui";
 	public static final String SWAGGER_UI_JAR_PREFIX = "META-INF/resources/webjars/swagger-ui/2.2.10";
@@ -46,39 +42,6 @@ public class SwaggerUIHandler extends AbstractHandler {
 		} else {
 			return serveFile("/" + path, myHostname);
 		}
-	}
-
-	protected Response serveFile(String path, String myHostname) throws IOException {
-		// serve files from swagger-ui jar
-		if (!path.startsWith(ROOT_PATH)) {
-			logger.info("File request blocked: '" + path + "'");
-			return Response.status(Status.FORBIDDEN).build();
-		}
-		String filename = path.replace(ROOT_PATH + "/", SWAGGER_UI_JAR_PREFIX + "/");
-		InputStream is = getClass().getClassLoader().getResourceAsStream(filename);
-		if (is == null) {
-			logger.info("File not found: '" + filename + "'");
-			return Response.status(Status.NOT_FOUND).build();
-		}
-		byte[] bytes = SimpleTools.toByteArray(is);
-		String lName = filename.toLowerCase();
-		String mime = "application/octet-stream";
-		if (lName.endsWith(".html") || lName.endsWith(".htm")) {
-			mime = "text/html";
-		} else if (lName.endsWith(".css")) {
-			mime = "text/css";
-		} else if (lName.endsWith(".js")) {
-			mime = "text/javascript";
-		} else if (lName.endsWith(".json")) {
-			mime = "application/json";
-		} else if (lName.endsWith(".ico")) {
-			mime = "image/x-icon";
-		} else if (lName.endsWith(".png")) {
-			mime = "image/png";
-		} else {
-			logger.log(Level.WARNING, "Unknown file type '" + filename + "' using " + mime + " as mime");
-		}
-		return Response.ok(bytes).type(mime).build();
 	}
 
 }
