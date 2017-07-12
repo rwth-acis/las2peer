f54608a6dd3a55c914e5ffabd5f23bcfd4e3044c
Jasper Nalbach <nalbach@dbis.rwth-aachen.de>
2017-07-12 15:56:02 +0200

    improve PAstryNodeImplTEst

--- core/src/test/java/i5/las2peer/p2p/PastryNodeImplTest.java
+++ core/src/test/java/i5/las2peer/p2p/PastryNodeImplTest.java
@@ -1,5 +1,7 @@
 package i5.las2peer.p2p;
 
+import java.util.ArrayList;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,26 +13,57 @@ public class PastryNodeImplTest {
 
 	@Test
 	public void testSystemDefinedPort() {
+		PastryNodeImpl testNode = null;
 		try {
-			PastryNodeImpl testNode = TestSuite.launchNetwork(1).get(0);
+			testNode = TestSuite.launchNetwork(1).get(0);
 			Assert.assertNotEquals(0, testNode.getPort());
 		} catch (Exception e) {
 			e.printStackTrace();
 			Assert.fail(e.toString());
+		} finally {
+			testNode.shutDown();
+			System.out.println("Node stopped");
 		}
 	}
 
 	@Test
 	public void testNodeRestart() {
+		PastryNodeImpl testNode = null;
 		try {
-			PastryNodeImpl testNode = TestSuite.launchNetwork(1).get(0);
+			testNode = TestSuite.launchNetwork(1).get(0);
 			testNode.shutDown();
 			testNode.launch();
 			testNode.shutDown();
 		} catch (Exception e) {
 			e.printStackTrace();
 			Assert.fail(e.toString());
+		} finally {
+			testNode.shutDown();
+			System.out.println("Node stopped");
 		}
+	}
+
+	@Test
+	public void testStartNetwork() {
+		ArrayList<PastryNodeImpl> nodes = null;
+		try {
+			// start nodes
+			nodes = TestSuite.launchNetwork(3);
+			System.out.println("Test network started");
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail(e.getMessage());
+		} finally {
+			// stop nodes
+			if (nodes != null) {
+				for (PastryNodeImpl node : nodes) {
+					node.shutDown();
+					System.out.println("Node " + node.getPort() + " stopped");
+				}
+				nodes = null;
+			}
+		}
+
 	}
 
 }
