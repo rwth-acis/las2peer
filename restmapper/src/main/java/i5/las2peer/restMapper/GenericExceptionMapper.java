2cb8c1b4c3cb2dd268aaf8c141cd333cb7b8f695
Thomas Cujé <tcuje@freenet.de>
2017-08-09 16:59:39 +0200

    improve auto exception mapping in GenericExceptionMapper

--- restmapper/src/main/java/i5/las2peer/restMapper/GenericExceptionMapper.java
+++ restmapper/src/main/java/i5/las2peer/restMapper/GenericExceptionMapper.java
@@ -2,6 +2,10 @@ package i5.las2peer.restMapper;
 
 import java.util.logging.Level;
 
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.ForbiddenException;
+import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -9,6 +13,10 @@ import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import i5.las2peer.api.execution.ResourceNotFoundException;
+import i5.las2peer.api.execution.ServiceAccessDeniedException;
+import i5.las2peer.api.execution.InvocationBadArgumentException;
+import i5.las2peer.api.execution.ServiceInvocationException;
 import i5.las2peer.logging.L2pLogger;
 
 @Provider
@@ -21,6 +29,18 @@ public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
 		// FIXME use format from WebConnector#sendUnexpectedErrorResponse
 		logger.log(Level.INFO, "Request failed: " + e.toString(), e);
 		int code = Status.INTERNAL_SERVER_ERROR.getStatusCode();
+		// map ServiceInvocationExceptions to WebApplicationExceptions
+		if (e instanceof ServiceInvocationException) {
+			if (e instanceof InvocationBadArgumentException) {
+				e = new BadRequestException(e.getMessage(), e.getCause());
+			} else if (e instanceof ResourceNotFoundException) {
+				e = new NotFoundException(e.getMessage(), e.getCause());
+			} else if (e instanceof ServiceAccessDeniedException) {
+				e = new ForbiddenException(e.getMessage(), e.getCause());
+			} else {
+				e = new InternalServerErrorException("Exception during RMI call", e);
+			}
+		}
 		if (e instanceof WebApplicationException) {
 			WebApplicationException webEx = (WebApplicationException) e;
 			code = webEx.getResponse().getStatus();
