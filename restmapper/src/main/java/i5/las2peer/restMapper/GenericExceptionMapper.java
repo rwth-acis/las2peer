4c58c28f536f039350d4afe3550f70963a919f6a
Thomas Cujé <tcuje@freenet.de>
2017-09-05 09:36:24 +0200

    add ServiceNotAuthorizedException to GenericExceptionMapper

--- restmapper/src/main/java/i5/las2peer/restMapper/GenericExceptionMapper.java
+++ restmapper/src/main/java/i5/las2peer/restMapper/GenericExceptionMapper.java
@@ -5,6 +5,7 @@ import java.util.logging.Level;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
@@ -13,10 +14,11 @@ import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import i5.las2peer.api.execution.InvocationBadArgumentException;
 import i5.las2peer.api.execution.ResourceNotFoundException;
 import i5.las2peer.api.execution.ServiceAccessDeniedException;
-import i5.las2peer.api.execution.InvocationBadArgumentException;
 import i5.las2peer.api.execution.ServiceInvocationException;
+import i5.las2peer.api.execution.ServiceNotAuthorizedException;
 import i5.las2peer.logging.L2pLogger;
 
 @Provider
@@ -35,6 +37,8 @@ public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
 				e = new BadRequestException(e.getMessage(), e.getCause());
 			} else if (e instanceof ResourceNotFoundException) {
 				e = new NotFoundException(e.getMessage(), e.getCause());
+			} else if (e instanceof ServiceNotAuthorizedException) {
+				e = new NotAuthorizedException(e.getMessage(), e.getCause());
 			} else if (e instanceof ServiceAccessDeniedException) {
 				e = new ForbiddenException(e.getMessage(), e.getCause());
 			} else {
