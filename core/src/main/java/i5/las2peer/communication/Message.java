589b0747212214aac02a7c6c5817671dfa47982a
Thomas Cujé <tcuje@freenet.de>
2017-09-22 21:38:00 +0200

    add message constructor check for anonymous

--- core/src/main/java/i5/las2peer/communication/Message.java
+++ core/src/main/java/i5/las2peer/communication/Message.java
@@ -258,6 +258,8 @@ public class Message implements XmlAble, Cloneable {
 			throws EncodingFailedException, InternalSecurityException, SerializationException {
 		if (from == null) {
 			throw new IllegalArgumentException("null not allowed as sender!");
+		} else if (from instanceof AnonymousAgent) {
+			throw new IllegalArgumentException("Anonymous not allowed as message sender!");
 		}
 
 		sender = from;
