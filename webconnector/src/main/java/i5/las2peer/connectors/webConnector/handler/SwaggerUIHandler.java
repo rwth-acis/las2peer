19a6aff8dbe0b8f10fbf29e060d107d177ae2606
Thomas Cujé <tcuje@freenet.de>
2017-11-12 11:54:24 +0100

    add SwaggerUIHandler mime type detection

--- webconnector/src/main/java/i5/las2peer/connectors/webConnector/handler/SwaggerUIHandler.java
+++ webconnector/src/main/java/i5/las2peer/connectors/webConnector/handler/SwaggerUIHandler.java
@@ -9,12 +9,14 @@ import java.nio.charset.StandardCharsets;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.apache.commons.io.IOUtils;
+
 import i5.las2peer.connectors.webConnector.WebConnector;
 import i5.las2peer.logging.L2pLogger;
-import i5.las2peer.tools.SimpleTools;
 
 @Path(SwaggerUIHandler.RESOURCE_PATH)
 public class SwaggerUIHandler extends AbstractFileHandler {
@@ -47,24 +49,25 @@ public class SwaggerUIHandler extends AbstractFileHandler {
 	@Override
 	protected Response serveFile(String filename) throws IOException {
 		String resourceName = "/META-INF/resources/webjars/swagger-ui/2.2.10/" + filename;
-		InputStream is = getClass().getResourceAsStream(resourceName);
-		if (is == null) {
-			logger.info("File not found: '" + resourceName + "'");
-			return Response.status(Status.NOT_FOUND).build();
-		}
-		byte[] bytes = SimpleTools.toByteArray(is);
-		String strContent = new String(bytes, StandardCharsets.UTF_8);
 		if (filename.equalsIgnoreCase("index.html")) {
+			InputStream is = getClass().getResourceAsStream(resourceName);
+			if (is == null) {
+				logger.info("File not found: '" + resourceName + "'");
+				return Response.status(Status.NOT_FOUND).build();
+			}
+			String strContent = IOUtils.toString(is, StandardCharsets.UTF_8);
 			String oidcClientId = connector.getOidcClientId();
 			if (oidcClientId != null) {
 				strContent = strContent.replace("clientId: \"your-client-id\",", "clientId: \"" + oidcClientId + "\",");
 			}
 			String oidcClientSecret = connector.getOidcClientSecret();
-			strContent = strContent.replace("clientSecret: \"your-client-secret-if-required\",",
-					"clientSecret: \"" + oidcClientSecret + "\",");
-			return Response.ok(strContent).build();
+			if (oidcClientSecret != null) {
+				strContent = strContent.replace("clientSecret: \"your-client-secret-if-required\",",
+						"clientSecret: \"" + oidcClientSecret + "\",");
+			}
+			return Response.ok(strContent, MediaType.TEXT_HTML).build();
 		} else {
-			return Response.ok(bytes).build();
+			return super.serveFile(resourceName);
 		}
 	}
 }
