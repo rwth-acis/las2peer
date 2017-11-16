26f403a882587e156be3d02714dba25ae6cc5257
Thomas Cujé <tcuje@freenet.de>
2017-11-16 18:03:40 +0100

    fix typo

--- core/src/main/java/i5/las2peer/api/security/Agent.java
+++ core/src/main/java/i5/las2peer/api/security/Agent.java
@@ -1,12 +1,13 @@
 package i5.las2peer.api.security;
 
 /**
- * Represents an acting entitiy in the network.
+ * Represents an acting entity in the network.
  * 
  * It can have two states: Locked agents cannot used to perform any actions while unlocked agents are able to do so.
  *
  */
 public interface Agent {
+
 	/**
 	 * The agent id
 	 * 
@@ -20,4 +21,5 @@ public interface Agent {
 	 * @return true if the agent is unlocked.
 	 */
 	public boolean isLocked();
+
 }
