539b771feeb0ad1e18bfeaac214cd894c85d9034
AlexanderNeumann <alexander.t.n@web.de>
2018-07-17 14:37:23 +0200

    Bot Content Generator interface

--- core/src/main/java/i5/las2peer/logging/bot/BotContentGenerator.java
+++ core/src/main/java/i5/las2peer/logging/bot/BotContentGenerator.java
@@ -2,10 +2,10 @@ package i5.las2peer.logging.bot;
 
 public interface BotContentGenerator {
 
-	public boolean trainStep(String input, String output);
+	public boolean trainStep(String out_dir, String input, String output);
 
-	public boolean train(String out_dir, double learning_rate, int num_training_steps);
+	public boolean train(String out_dir, String unit, double learning_rate, int num_training_steps, int epochs);
 
-	public Object inference(String input);
+	public Object inference(String out_dir, String input);
 
 }
