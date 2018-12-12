bf877cec0c907dd56978961bcd7106e78dcd00b1
Tom Janson <tom.janson@rwth-aachen.de>
2018-12-13 00:04:57 +0100

    add release data comparison operators for dup check

--- core/src/main/java/i5/las2peer/registry/data/ServiceReleaseData.java
+++ core/src/main/java/i5/las2peer/registry/data/ServiceReleaseData.java
@@ -1,6 +1,8 @@
 package i5.las2peer.registry.data;
 
 import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class ServiceReleaseData {
 	private String serviceName;
@@ -40,5 +42,24 @@ public class ServiceReleaseData {
 	@Override
 	public String toString() {
 		return "ServiceReleaseData(service name: " + this.getServiceName() + ", version: " + this.getVersion() + ")";
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		ServiceReleaseData that = (ServiceReleaseData) o;
+		return Objects.equals(serviceName, that.serviceName) &&
+				Objects.equals(versionMajor, that.versionMajor) &&
+				Objects.equals(versionMinor, that.versionMinor) &&
+				Objects.equals(versionPatch, that.versionPatch) &&
+				Arrays.equals(signature, that.signature);
+	}
+
+	@Override
+	public int hashCode() {
+		int result = Objects.hash(serviceName, versionMajor, versionMinor, versionPatch);
+		result = 31 * result + Arrays.hashCode(signature);
+		return result;
 	}
 }
