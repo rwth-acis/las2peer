614f6e8875e52f168daf02292b19897384bb63a4
Michał Słupczyński <michal.slupczynski@rwth-aachen.de>
2019-10-29 18:23:50 +0100

    typo fix

--- core/src/main/java/i5/las2peer/p2p/EthereumNode.java
+++ core/src/main/java/i5/las2peer/p2p/EthereumNode.java
@@ -204,7 +204,7 @@ public class EthereumNode extends PastryNodeImpl {
 			
 			logger.info("ETH   DATA: " + userInBlockchain.getAgentId().toString() + " | " + userInBlockchain.getPublicKey().toString());
 			logger.info("MATCHING ID? " + String.valueOf(userInBlockchain.getAgentId().equals(agent.getIdentifier())));
-			logger.info("MATCHING PubKey? " + String.valueOf(userInBlockchain.getPublicKey().equals(agent.getPublicKey()));
+			logger.info("MATCHING PubKey? " + String.valueOf(userInBlockchain.getPublicKey().equals(agent.getPublicKey())));
 			// damn, we might not be able to compare the ethereum address, because it may be null if the agent is locked
 			// does it matter? I guess not. if name and pubkey match, do we care who the owner address is?
 			// let's go with no. TODO: consider implications
