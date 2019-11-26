09208704f2c02e06f325bd8a4349751fb947d11b
Alexander <alexander.t.n@web.de>
2019-11-26 14:06:11 +0100

    Clean start

--- restmapper/src/test/java/i5/las2peer/restMapper/services/TestBean.java
+++
@@ -1,9 +0,0 @@
-package i5.las2peer.restMapper.services;
-
-public class TestBean {
-	public String name;
-
-	public TestBean(String name) {
-		this.name = name;
-	}
-}
--- webconnector/fix_symlink.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-echo "unlinking old symlink"
-touch resources/webapp
-unlink resources/webapp
-echo "linking new build"
-ln -svf "$(pwd)/frontend/build/es6-bundled" "$(pwd)/resources/webapp"
