9cd96c648e9b1279dc63ebd10462e31b43bba066
Rastoder Erdzan <rastoder.erdzan@rwth-aachen.de>
2021-07-13 20:09:50 +0200

    clientid

--- webconnector/frontend/src/node-frontend.js
+++ webconnector/frontend/src/node-frontend.js
@@ -218,7 +218,7 @@ class NodeFrontend extends PolymerElement {
           </template>
             <las2peer-frontend-statusbar id="statusbar"
               base-url="[[hostUrl]]" service="las2peer Node Front-End"
-              oidcclientid="bdda7396-3f6d-4d83-ac21-65b4069d0eab"
+              oidcclientid="debugLas2peer"
               oidcpopupsigninurl$="[[_loadUrl]]"
               oidcpopupsignouturl$="[[_loadUrl]]"
               oidcsilentsigninurl$="[[_loadUrl]]"
