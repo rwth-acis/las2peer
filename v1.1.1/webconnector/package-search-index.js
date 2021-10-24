70e436d9063c671c2542f9dbd13fe6f4efa75a3e
Philipp Dolif <git.phil.cd@gmail.com>
2021-10-24 09:50:00 +0200

    Store Javadoc since v1.1.0

---
+++ v1.1.1/webconnector/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"i5.las2peer.connectors.webConnector"},{"l":"i5.las2peer.connectors.webConnector.client"},{"l":"i5.las2peer.connectors.webConnector.handler"},{"l":"i5.las2peer.connectors.webConnector.util"}]
